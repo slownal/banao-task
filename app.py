479dcd0ee0a43def402dd41e594d17964f5b0434
Sonal Agnihotri <agnihotrisonal749@gmail.com>
2025-06-20 11:41:17 +0530

    new change

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from handler import send_email as send_email_handler
 import json
+import os
 
 app = Flask(__name__)
 
@@ -10,13 +11,13 @@ def webhook():
         'body': json.dumps(request.json)
     }
     context = {}
-    
     response = send_email_handler(event, context)
-    
     return jsonify(json.loads(response['body'])), response['statusCode']
 
 @app.route('/', methods=['GET'])
 def home():
     return "Email API is running!", 200
+
 if __name__ == "__main__":
-    app.run(port=5001) 
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
